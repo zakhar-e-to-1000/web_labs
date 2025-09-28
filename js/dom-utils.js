279af9dd6db5a70bca47eaa4b57446d091c1b879
Zakhar <super.korustyvach@gmail.com>
2025-09-28 22:44:14 +0300

    update: mess but works

--- js/dom-utils.js
+++ js/dom-utils.js
@@ -7,30 +7,46 @@ const create = document.getElementById("create");
 const edit = document.getElementById("edit");
 const view = document.getElementById("view");
 
+const header__search = document.getElementById("header__search");
 
-const header_create = document.getElementById("header__create")
-const create_go_back = document.getElementById("create__go-back");
-const edit_go_back = document.getElementById("edit__go-back");
 
 const create__name = document.getElementById("create__name");
 const create__duration = document.getElementById("create__duration");
 const create__reviews = document.getElementById("create__reviews");
 
 
+const edit__id = document.getElementById("edit__id");
+const edit__name = document.getElementById("edit__name");
+const edit__duration = document.getElementById("edit__duration");
+const edit__reviews = document.getElementById("edit__reviews");
 
-header_create.addEventListener('click', () => {
+
+const sort__pannel = document.getElementById("header__sort-pannel");
+
+
+
+export function goCreate() {
+    header__search.value = ''
     view.classList.remove("open");
     edit.classList.remove("open");
     create.classList.add("open");
-    clearCreateInputs();
-})
-create_go_back.addEventListener("click", goBack);
-edit_go_back.addEventListener("click", goBack);
+    sort__pannel.classList.remove("open");
+
+}
 
 export function goBack() {
     create.classList.remove("open");
     edit.classList.remove("open");
     view.classList.add("open");
+    sort__pannel.classList.add("open");
+}
+
+export function goEdit() {
+    header__search.value = ''
+    create.classList.remove("open");
+    view.classList.remove("open");
+    sort__pannel.classList.remove("open");
+    edit.classList.add('open')
 }
 
 export function clearCreateInputs() {
@@ -45,6 +61,22 @@ export function getCreateInputs() {
         duration: create__duration.valueAsNumber,
         reviews: create__reviews.valueAsNumber
     }
+}
+
+export function getEditInputs() {
+    return {
+        id: edit__id.value,
+        name: edit__name.value,
+        duration: edit__duration.valueAsNumber,
+        reviews: edit__reviews.valueAsNumber
+    }
+}
+
+export function fillEditInputs(film_obj) {
+    edit__id.value = film_obj.id;
+    edit__name.value = film_obj.name;
+    edit__duration.value = film_obj.duration;
+    edit__reviews.value = film_obj.reviews;
 }
 
 export function getFilmObj({ name, duration, reviews }) {
@@ -64,14 +96,44 @@ function cardTemplate({ id, name, duration, reviews }) {
     </div>`
 }
 
-export function createCard(film_obj) {
-    const template = cardTemplate(film_obj);
-    grid_parent.insertAdjacentHTML("afterbegin", template);
+export function renderCard({ id, name, duration, reviews }, callback_edit, callback_delete) {
+    // const template = cardTemplate(film_obj);
+    const card = document.createElement("div");
+    card.classList.add('card');
+    card.id = id;
+    const p1 = document.createElement("p");
+    p1.classList.add("card__title")
+    p1.innerText = name
+    card.appendChild(p1)
+    const p2 = document.createElement("p");
+    p2.classList.add("card__duration")
+    p2.innerText = `${duration} minutes`
+    card.appendChild(p2)
+    const p3 = document.createElement("p");
+    p3.innerText = `${reviews} reviews`
+    p3.classList.add("card__review")
+    card.appendChild(p3)
+
+    const close = document.createElement("button");
+    close.classList.add("card__close")
+    close.textContent = 'Delete'
+    card.appendChild(close);
+
+    grid_parent.appendChild(card)
+
+    close.addEventListener('click', (e) => {
+        e.stopPropagation();
+        callback_delete(id);
+    })
+    card.addEventListener('click', () => {
+        callback_edit(id);
+    })
+    // grid_parent.insertAdjacentHTML("afterbegin", template);
 }
 
-export function renderList(film_list) {
+export function renderList(film_list, callback_edit, callback_delete) {
     grid_parent.innerHTML = ''
     for (let film of film_list) {
-        createCard(film)
+        renderCard(film, callback_edit, callback_delete)
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 "use strict"
-import { goBack, createCard, getFilmObj, getCreateInputs, clearCreateInputs, renderList } from "./dom-utils.js"
+import {
+    goBack, goEdit, goCreate,
+    getFilmObj, getCreateInputs, getEditInputs,
+    clearCreateInputs, renderList, fillEditInputs
+} from "./dom-utils.js"
 import { openDialog } from "./dialog-setup.js"
 
 const list = []
@@ -8,18 +12,26 @@ const template = {
     duration: 61,
     reviews: 60000
 }
-console.log(template);
-// for (let i = 0; i < 10; i++) {
-//     const film_obj = getFilmObj(template)
-//     createCard(film_obj);
-//     console.log(uuid.v1());
-// }
 
 const filmList = [];
 
-const header__sort = document.getElementById('header__sort')
-const create_button = document.getElementById("create__create");
+const header_create = document.getElementById("header__create")
 const header__search = document.getElementById("header__search");
+const header__sort = document.getElementById('header__sort');
+const create_button = document.getElementById("create__create");
+const edit_button = document.getElementById("edit__edit");
+
+
+const create_go_back = document.getElementById("create__go-back");
+const edit_go_back = document.getElementById("edit__go-back");
+create_go_back.addEventListener("click", goBack);
+edit_go_back.addEventListener("click", goBack);
+
+
+header_create.addEventListener('click', () => {
+    goCreate()
+    clearCreateInputs();
+})
 
 header__sort.addEventListener('click', () => {
     header__sort.classList.toggle('checked');
@@ -31,7 +43,6 @@ header__search.addEventListener("keyup", (e) => {
         return;
     }
     updateFilmView()
-    console.log("hello")
 })
 
 create_button.addEventListener('click', () => {
@@ -45,12 +56,28 @@ create_button.addEventListener('click', () => {
     filmList.unshift(film_obj)
 
     updateFilmView();
-    console.log(filmList);
+    goBack();
+})
+
+edit_button.addEventListener('click', () => {
+    const input = getEditInputs();
+    filmList.forEach((value, index, arr) => {
+        if (value.id == input.id) {
+            arr[index] = input;
+        }
+    });
+    updateFilmView();
     goBack();
 })
 
 function updateFilmView() {
     const prefix = header__search.value;
+    const label = document.getElementById("view__title");
+    if (prefix === "") {
+        label.innerText = 'Films';
+    } else {
+        label.innerText = `Search: "${prefix}"`
+    }
     const sort_cond = header__sort.classList.contains('checked')
     const searchResult = filmList.filter((value) => {
         return value.name.startsWith(prefix)
@@ -58,5 +85,34 @@ function updateFilmView() {
     if (sort_cond) {
         searchResult.sort((a, b) => (a.duration || Infinity) - (b.duration || Infinity));
     }
-    renderList(searchResult);
+    renderList(searchResult, onEdit, onDelete);
+}
+
+function onEdit(card_id) {
+    const card = checkFilm(card_id);
+    if (card === null) {
+        return
+    }
+    fillEditInputs(card)
+    goEdit()
+}
+
+function onDelete(card_id) {
+    console.log("delete");
+    for (let index in filmList) {
+        if (filmList[index].id === card_id) {
+            filmList.splice(index);
+            updateFilmView();
+            return;
+        }
+    }
+}
+
+function checkFilm(card_id) {
+    for (let film of filmList) {
+        if (film.id === card_id) {
+            return film;
+        }
+    }
+    return null
 }
