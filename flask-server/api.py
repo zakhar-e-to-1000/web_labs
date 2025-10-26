f35f47d36fa1ce774f3369948aaadbea5f9e660f
Zakhar <super.korustyvach@gmail.com>
2025-10-26 12:56:14 +0200

    update: kinda works now

--- flask-server/api.py
+++ flask-server/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, url_for, send_file, request
 import sqlite3
 app = Flask(__name__, static_url_path="/", static_folder='frontend')
+DATABASE_PATH = 'database.db'
 
 
 def dict_factory(cursor, row):
@@ -9,13 +10,13 @@ def dict_factory(cursor, row):
 
 
 def init_db():
-    con = sqlite3.connect('database.db')
+    con = sqlite3.connect(DATABASE_PATH)
     cur = con.cursor()
     # cur.execute('DROP TABLE film')
     cur.execute('''
     CREATE TABLE IF NOT EXISTS film
     (id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name VARCHAR(255),
+    title TEXT,
     duration INT,
     reviews INT,
     description TEXT) STRICT
@@ -28,11 +29,21 @@ def db_select_all(cur: sqlite3.Cursor):
     return res.fetchall()
 
 
+def db_select(cur: sqlite3.Cursor, id):
+    res = cur.execute('''
+        SELECT * FROM film
+        WHERE id=?
+    ''', (id,))
+    return res.fetchone()
+
+
 def db_insert(cur: sqlite3.Cursor, params):
     cur.execute('''
-    INSERT INTO film (name, duration, reviews)
-    VALUES (:name, :duration, :reviews)
+    INSERT INTO film (title, duration, reviews, description)
+    VALUES (:title, :duration, :reviews, :description)
 ''', (params))
+    params['id'] = cur.lastrowid
+    return params
 
 
 def db_delete(cur: sqlite3.Cursor, id):
@@ -41,12 +52,13 @@ def db_delete(cur: sqlite3.Cursor, id):
 ''', (int(id),))
 
 
-def db_update(cur: sqlite3.Cursor, params):
+def db_put(cur: sqlite3.Cursor, params):
     cur.execute('''
     UPDATE film
-    SET name = :name,
+    SET title = :title,
         duration = :duration,
         reviews = :reviews
+        description = :description
     WHERE id = :id
 ''', params)
 
@@ -56,16 +68,16 @@ def index():
     return send_file('frontend/index.html')
 
 
-@app.route('/film', methods=['GET'])
+@app.route('/api/films', methods=['GET'])
 def films_get():
     preffix = request.args.get('s', '', str)
     sort_param = request.args.get('sort', 'none', str)
     # app.logger.info(str(preffix)+' '+str(sort_param))
-    con = sqlite3.connect('database.db')
+    con = sqlite3.connect(DATABASE_PATH)
     con.row_factory = dict_factory
     cur = con.cursor()
     films = db_select_all(cur)
-    res = filter(lambda e: e['name'].lower().startswith(
+    res = filter(lambda e: e['title'].lower().startswith(
         preffix.strip().lower()), films)
     con.close()
     if sort_param != 'none':
@@ -73,21 +85,30 @@ def films_get():
     return list(res)
 
 
-@app.route('/film', methods=['POST'])
+@app.route('/api/films', methods=['POST'])  # type: ignore
 def films_post():
     film_params = request.json
-    con = sqlite3.connect('database.db')
+    con = sqlite3.connect(DATABASE_PATH)
     cur = con.cursor()
     # app.logger.debug(f'A value for debugging {film_params}')
-    db_insert(cur, film_params)
+    inserted = db_insert(cur, film_params)
     con.commit()
     con.close()
-    return ("ok", 200)
+    return inserted, 201
 
 
-@app.route('/film/<int:film_id>', methods=['DELETE'])
+@app.route('/api/films/<int:film_id>', methods=['GET'])  # type: ignore
+def film_get(film_id):
+    con = sqlite3.connect(DATABASE_PATH)
+    con.row_factory = dict_factory
+    cur = con.cursor()
+    res = db_select(cur, film_id)
+    return res, 200
+
+
+@app.route('/api/films/<int:film_id>', methods=['DELETE'])
 def film_delete(film_id):
-    con = sqlite3.connect('database.db')
+    con = sqlite3.connect(DATABASE_PATH)
     cur = con.cursor()
     db_delete(cur, film_id)
     con.commit()
@@ -95,18 +116,18 @@ def film_delete(film_id):
     return ('ok', 200)
 
 
-@app.route('/film/<int:film_id>', methods=['PUT'])
+@app.route('/api/films/<int:film_id>', methods=['PUT'])
 def film_update(film_id):
     film_params = request.json
     if film_params != None:
         film_params['id'] = film_id
-    con = sqlite3.connect('database.db')
+    con = sqlite3.connect(DATABASE_PATH)
     cur = con.cursor()
-    db_update(cur, film_params)
+    db_put(cur, film_params)
     con.commit()
     con.close()
     return ('ok', 200)
 
 
 if __name__ == '__main__':
-    init_db()
+    app.run()
--- flask-server/create_database.py
+++ flask-server/create_database.py
@@ -1,4 +1,74 @@
-from api import app, db
+import sqlite3
+from api import db_insert
+import os
+import random
+DATABASE_PATH = 'database.db'
 
-with app.app_context():
-    db.create_all()
+
+def random_lorem():
+    lorem = "Curabitur ac magna ut enim elementum mattis. Integer sit amet metus nec urna lacinia rhoncus sed quis odio. Cras nisi dui, vulputate tincidunt convallis at, aliquet non turpis. Donec tincidunt velit eu nisi interdum, eget pharetra erat tempor. Vivamus lobortis nunc non nibh congue, non iaculis erat dapibus. Aenean mattis, lorem nec dictum aliquam, urna quam tempus metus, nec efficitur tortor tortor ut felis. Cras eu tristique diam, semper convallis libero. Vivamus id ante lectus. Morbi pulvinar eros hendrerit sapien dignissim facilisis. Praesent vitae justo ac arcu consequat volutpat nec sit amet magna. Phasellus dignissim egestas augue quis lobortis. Mauris in viverra sem. Vestibulum eu suscipit neque. Duis non est lorem. Duis mattis tortor sit amet volutpat vehicula. Duis dolor massa, auctor non molestie ac, convallis quis risus. "
+    start = random.randint(0, len(lorem)//2)
+    return lorem[start:]
+
+
+def init_db():
+    con = sqlite3.connect(DATABASE_PATH)
+    cur = con.cursor()
+    # cur.execute('DROP TABLE film')
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS film
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    duration INT,
+    reviews INT,
+    description TEXT) STRICT
+''')
+    con.close()
+
+
+def main():
+    if os.path.exists(DATABASE_PATH):
+        os.remove(DATABASE_PATH)
+    init_db()
+    con = sqlite3.connect(DATABASE_PATH)
+    cur = con.cursor()
+    num = 100
+    for i in range(num):
+        suffix = chr(ord('a')+(i % 10))+chr(ord('a')+(i // 10))
+        name = "Film "+suffix
+        params = {
+            'title': name,
+            'duration': random.randint(1, 240),
+            'reviews': random.randint(1_000, 100_000),
+            'description': random_lorem()
+        }
+
+        db_insert(cur, params)
+    con.commit()
+    con.close()
+
+
+if __name__ == '__main__':
+    main()
+
+# function randomLorem() {
+#     const lorem = "Curabitur ac magna ut enim elementum mattis. Integer sit amet metus nec urna lacinia rhoncus sed quis odio. Cras nisi dui, vulputate tincidunt convallis at, aliquet non turpis. Donec tincidunt velit eu nisi interdum, eget pharetra erat tempor. Vivamus lobortis nunc non nibh congue, non iaculis erat dapibus. Aenean mattis, lorem nec dictum aliquam, urna quam tempus metus, nec efficitur tortor tortor ut felis. Cras eu tristique diam, semper convallis libero. Vivamus id ante lectus. Morbi pulvinar eros hendrerit sapien dignissim facilisis. Praesent vitae justo ac arcu consequat volutpat nec sit amet magna. Phasellus dignissim egestas augue quis lobortis. Mauris in viverra sem. Vestibulum eu suscipit neque. Duis non est lorem. Duis mattis tortor sit amet volutpat vehicula. Duis dolor massa, auctor non molestie ac, convallis quis risus. "
+#     let rand = Math.floor(Math.random() / 2 * lorem.length)
+#     return lorem.slice(rand)
+# }
+
+# export default function getFilms(count) {
+#     const films = []
+#     for (let num=1
+#          num < count + 1
+#          num++) {
+#         films.push({
+#             title: `Film ${num}`,
+#             id: num,
+#             duration: Math.floor(Math.random() * 240),
+#             reviews: Math.floor(Math.random() * 10_000),
+#             description: randomLorem()
+#         })
+#     }
+#     return films
+# }
