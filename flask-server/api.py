cb36257e926f6892b37b1662e4c25ba35dfb0e1a
Zakhar <super.korustyvach@gmail.com>
2025-10-26 12:02:03 +0200

    reverted to previous backend

--- flask-server/api.py
+++ flask-server/api.py
@@ -1,78 +1,112 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Resource, Api, reqparse, fields, marshal_with, abort
-from sqlalchemy import select
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
-api = Api(app)
+from flask import Flask, url_for, send_file, request
+import sqlite3
+app = Flask(__name__, static_url_path="/", static_folder='frontend')
 
 
-class FilmModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    duration = db.Column(db.Integer)
-    reviews = db.Column(db.Integer)
-    description = db.Column(db.Text)
-
-    def __repr__(self) -> str:
-        return f"Film(title = {self.title} duration={self.duration} reviews={self.reviews} description={self.description})"
+def dict_factory(cursor, row):
+    fields = [column[0] for column in cursor.description]
+    return {key: value for key, value in zip(fields, row)}
 
 
-film_args = reqparse.RequestParser()
-film_args.add_argument('title', type=str, required=True,
-                       help="title can't be blank")
-film_args.add_argument('duration', type=int)
-film_args.add_argument('reviews', type=int)
-film_args.add_argument('description', type=str)
-
-userFields = {
-    'id': fields.Integer,
-    'title': fields.String,
-    'duration': fields.Integer,
-    'reviews': fields.Integer,
-    'description': fields.String
-}
-
-
-class Films(Resource):
-    @marshal_with(userFields)
-    def get(self):
-        # films = db.select(FilmModel)
-        films = db.session.scalars(db.select(FilmModel)).all()
-        # films = FilmModel.query.all()
-        return films
-
-    @marshal_with(userFields)
-    def post(self):
-        args = film_args.parse_args()
-        film = FilmModel(
-            title=args['title'], duration=args['duration'],
-            reviews=args['reviews'], description=args['description'])
-        db.session.add(film)
-        db.session.commit()
-        return film
+def init_db():
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    # cur.execute('DROP TABLE film')
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS film
+    (id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255),
+    duration INT,
+    reviews INT,
+    description TEXT) STRICT
+''')
+    con.close()
 
 
-class FilmItem(Resource):
-    @marshal_with(userFields)
-    def get(self, id):
-        film = db.session.scalar(
-            db.select(FilmModel).where(FilmModel.id == id))
-        return film
-
-    @marshal_with(userFields)
-    def put(self, id):
+def db_select_all(cur: sqlite3.Cursor):
+    res = cur.execute('SELECT * FROM film')
+    return res.fetchall()
 
 
-api.add_resource(Films, '/api/films')
-api.add_resource(FilmItem, '/api/films/<int:id>')
+def db_insert(cur: sqlite3.Cursor, params):
+    cur.execute('''
+    INSERT INTO film (name, duration, reviews)
+    VALUES (:name, :duration, :reviews)
+''', (params))
+
+
+def db_delete(cur: sqlite3.Cursor, id):
+    cur.execute('''
+    DELETE FROM film WHERE id = ?
+''', (int(id),))
+
+
+def db_update(cur: sqlite3.Cursor, params):
+    cur.execute('''
+    UPDATE film
+    SET name = :name,
+        duration = :duration,
+        reviews = :reviews
+    WHERE id = :id
+''', params)
 
 
 @app.route('/')
-def home():
-    return "heelo"
+def index():
+    return send_file('frontend/index.html')
+
+
+@app.route('/film', methods=['GET'])
+def films_get():
+    preffix = request.args.get('s', '', str)
+    sort_param = request.args.get('sort', 'none', str)
+    # app.logger.info(str(preffix)+' '+str(sort_param))
+    con = sqlite3.connect('database.db')
+    con.row_factory = dict_factory
+    cur = con.cursor()
+    films = db_select_all(cur)
+    res = filter(lambda e: e['name'].lower().startswith(
+        preffix.strip().lower()), films)
+    con.close()
+    if sort_param != 'none':
+        res = list(sorted(res, key=lambda e: e[sort_param]))
+    return list(res)
+
+
+@app.route('/film', methods=['POST'])
+def films_post():
+    film_params = request.json
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    # app.logger.debug(f'A value for debugging {film_params}')
+    db_insert(cur, film_params)
+    con.commit()
+    con.close()
+    return ("ok", 200)
+
+
+@app.route('/film/<int:film_id>', methods=['DELETE'])
+def film_delete(film_id):
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    db_delete(cur, film_id)
+    con.commit()
+    con.close()
+    return ('ok', 200)
+
+
+@app.route('/film/<int:film_id>', methods=['PUT'])
+def film_update(film_id):
+    film_params = request.json
+    if film_params != None:
+        film_params['id'] = film_id
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    db_update(cur, film_params)
+    con.commit()
+    con.close()
+    return ('ok', 200)
 
 
 if __name__ == '__main__':
-    app.run()
+    init_db()
