5b547aaa78c8c0d4ee444e69903f745e5cf3856b
Zakhar <super.korustyvach@gmail.com>
2025-11-30 20:20:27 +0200

    update: added ui for selecting product variations

--- my-app/src/pages/ItemPage/ItemPage.jsx
+++ my-app/src/pages/ItemPage/ItemPage.jsx
@@ -9,13 +9,20 @@ import get_film from "../../api/getFilm";
 import LoaderWheel from "../../components/LoaderWheel/LoaderWheel";
 import { addFilmToCart } from "../../api/filmCartSlice";
 import { useDispatch } from "react-redux";
+import PrimaryButton from '../../components/PrimaryButton/PrimaryButton'
+
 function ItemPage() {
     const { id } = useParams()
     const [film, setFilm] = useState()
     const [error, setError] = useState()
     const [loading, setLoading] = useState(true)
     const dispatch = useDispatch()
+    const [isDirector, setIsDirector] = useState(false)
 
+    let iconClassName = styles['film_icon']
+    if (isDirector) {
+        iconClassName += ' ' + styles['director-cut']
+    }
     useEffect(() => {
         get_film({
             id: id,
@@ -39,22 +46,30 @@ function ItemPage() {
                     film != undefined &&
                     <>
                         <div className={styles.main}>
-                            <img src="/src/assets/vintage-film-roll-stockcake.jpg" />
+                            <img src="/src/assets/vintage-film-roll-stockcake.jpg" className={iconClassName} />
                             <section className={styles['info-pannel']}>
                                 <h1>{film.title}</h1>
                                 <h2>Stats</h2>
                                 <p>Duration: {film.duration}</p>
                                 <p>Reviews: {film.reviews}</p>
+                                <div className={styles['select-group']}>
+                                    <PrimaryButton onClick={() => { setIsDirector(false) }}>🩶Normal</PrimaryButton>
+                                    <PrimaryButton onClick={() => { setIsDirector(true) }}>❤️Director Cut</PrimaryButton>
+                                </div>
+                                <PrimaryButton onClick={() => {
+                                    dispatch(addFilmToCart({
+                                        filmId: id,
+                                        filmCount: 1,
+                                        filmName: film.title,
+                                        variations: {
+                                            directorCut: isDirector
+                                        }
+                                    }))
+                                }}>Add to the cart</PrimaryButton>
                             </section>
                         </div>
                         <h2>Description</h2>
                         <p>{film.description}</p>
-                        <button onClick={() => {
-                            dispatch(addFilmToCart({
-                                filmId: id,
-                                filmCount: 1
-                            }))
-                        }}>Add to the cart</button>
 
                     </>
                 }
